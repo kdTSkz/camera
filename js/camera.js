841e4c267daf8a59e1bc9b6fb3ee5b590f2da83f
kdTSkz <33660166+kdTSkz@users.noreply.github.com>
2022-06-19 22:40:26 +0900

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -1,67 +1,81 @@
 (function () {
 
     document.addEventListener("DOMContentLoaded", function () {
+
         videoDevice();
+
         document.addEventListener("click", changeDevice);
     });
 
     let videoTrack = null;
     let videoDevices = null;
+    let deviceReady = true;
 
     const videoDevice = function (deviceId) {
 
         let promise = new Promise(function (resolve, reject) {
 
             let video = document.getElementById("video");
-            let container = video.parentNode;
 
-            if ((videoTrack === null) || (videoTrack.readyState == "ended")) {
+            if (deviceReady) {
 
-                let constraints = {
-                    audio: false,
-                    video: {
-                        width: container.clientWidth,
-                        height: container.clientHeight
+                if ((videoTrack === null) || (videoTrack.readyState == "ended")) {
+
+                    deviceReady = false;
+
+                    let container = video.parentNode;
+                    let constraints = {
+                        audio: false,
+                        video: {
+                            width: container.clientWidth,
+                            height: container.clientHeight
+                        }
+                    };
+
+                    if (container.clientWidth < container.clientHeight) {
+                        constraints.video.width = container.clientHeight;
+                        constraints.video.height = container.clientWidth;
                     }
-                };
 
-                if (container.clientWidth < container.clientHeight) {
-                    constraints.video.width = container.clientHeight;
-                    constraints.video.height = container.clientWidth;
-                }
+                    if (typeof deviceId === "undefined") {
+                        constraints.video.facingMode = "environment";
+                    } else {
+                        constraints.video.deviceId = deviceId;
+                    }
 
-                if (typeof deviceId === "undefined") {
-                    constraints.video.facingMode = "environment";
+                    navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+
+                        if (videoDevices === null) {
+
+                            navigator.mediaDevices.enumerateDevices().then(function (devices) {
+                                videoDevices = devices.filter(function (d) { return d.kind == "videoinput"; });
+                            });
+
+                            video.addEventListener("loadedmetadata", function () {
+
+                                video.classList.toggle("mirror", videoTrack.getSettings().facingMode == "user");
+                                video.play();
+
+                                deviceReady = true;
+
+                                resolve(video, true);
+                            });
+                        }
+
+                        videoTrack = stream.getVideoTracks()[0];
+                        video.srcObject = stream;
+
+                    }).catch(function (err) {
+                        console.log(err.name + ": " + err.message);
+                        reject(err);
+                    });
+
                 } else {
-                    constraints.video.deviceId = deviceId;
+                    resolve(video, true);
                 }
-
-                navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-
-                    if (videoDevices === null) {
-
-                        navigator.mediaDevices.enumerateDevices().then(function (devices) {
-                            videoDevices = devices.filter(function (d) { return d.kind == "videoinput"; });
-                        });
-
-                        video.addEventListener("loadedmetadata", function () {
-                            video.classList.toggle("mirror", videoTrack.getSettings().facingMode == "user");
-                            video.play();
-                        });
-                    }
-
-                    videoTrack = stream.getVideoTracks()[0];
-                    video.srcObject = stream;
-
-                    resolve(video);
-
-                }).catch(function (err) {
-                    console.log(err.name + ": " + err.message);
-                    reject(err);
-                });
 
             } else {
-                resolve(video);
+                resolve(video, false);
             }
         });
 
